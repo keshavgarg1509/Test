01bed440d5e84b44cab887d7ee39eb58cd525a80
keshavgarg1509 <code.keshavgarg@gmail.com>
2022-07-07 18:27:51 +0530

    change a file

--- index/admin.py
+++ index/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import about
 from .models import slider
 from .models import client
-# Register your models here.
+# Register your models here
 
 admin.site.register(about)
 admin.site.register(slider)
